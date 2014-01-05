343e13477f00351649168214da9d59f5270fa36f
Tim Caswell <tim@creationix.com>
2014-01-05 21:54:25 +0000

    Fix github config to work in IE

--- src/github-config.js
+++ src/github-config.js
@@ -1,16 +1,16 @@
-var origin = window.location.origin;
-if (origin === "http://localhost:8002") {
+var hostname = window.location.hostname;
+if (hostname === "localhost") {
   module.exports = {
     clientId: "700f0fceacdc16c17cf9",
     redirectUri: "http://localhost:8002/github-callback"
   };
 }
-else if (origin === "https://tedit.creationix.com") {
+else if (hostname === "tedit.creationix.com") {
   module.exports = {
     clientId: "f89769973f4842fde5bc",
     redirectUri: "https://tedit.creationix.com/github-callback"
   };
 }
 else {
-  throw new Error("Missing config for origin: " + origin);
+  throw new Error("Missing config for hostname: " + hostname);
 }
